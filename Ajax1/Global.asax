5782a5e91fcb04ba3a28a3f998bf54e9e4ee6580
Gunay <gunayisg@gmail.com>
2018-03-05 16:03:40 +0400

    Ajax is done

---
+++ Ajax1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ajax1.MvcApplication" Language="C#" %>
---
+++ Ajax1/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function() {
+    $("Select[name=Model]").change(function () {
+        var marka = $(this).val();
+        var Url = "https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/" + marka + "/modelyear/2015?format=json";
+        $.ajax({
+            url: Url,
+            type: "get",
+            dataType: "json",
+            success: function (response) {
+                //console.log(response)
+                var timing = response.Results;
+                for (var i = 0; i < timing.length; i++)
+                {
+                    $(".timing td").eq(i).text("test")
+                }
+            }
+        })
+
+
+    })
+})
